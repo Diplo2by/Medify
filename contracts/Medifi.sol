965137da020f91ab813c698ef892c5cc635acd20
Darshan V <81457012+Diplo2by@users.noreply.github.com>
2022-03-06 06:29:05 +0530

    Updated the sol contract
    added more fields and functions

--- contracts/Medifi.sol
+++ contracts/Medifi.sol
@@ -10,22 +10,31 @@ struct Record{ // the class/ solidity struct
 	uint pat_id; // patient id
 	string cid; //record cid of the ipfs data record
 	string name; // patient name
+	string blood_grp;
+	string vaccines;
+	string std;
+	string diabetes;
 }
+
 
 constructor() public {
 	
 }
 
-function UploadRecord(string memory _cid, string memory pat_id)public{
+function UploadRecord(string memory _cid, string memory pat_id, string memory _name,string memory _blood_grp, string memory _vaccines, string memory _std,string memory _diabetes )public{
 
 	require(bytes(_cid).length > 0); // check if there is a hash associated with a record
 	require(bytes(pat_id).length>0); // check for the patient id
 	require(msg.sender != address(0)); // make sure uploader is using an actual adress
 	Total_records++;
 
-	records [Total_records] = Record(Total_records,_cid,name);
+	records [Total_records] = Record(Total_records,_cid,_name,_blood_grp,_vaccines,_std,_diabetes);
 }
 function get(uint x) public view returns (string memory){
 	return records[x].cid;
+}
+function getAll(uint y) public view returns (Record memory){
+	return records[y];
+
 }
 }
